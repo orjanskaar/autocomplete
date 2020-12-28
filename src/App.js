c868f13a164cdbccfe10a74b797f8199e730b1d4
orjanskaar <54217257+orjanskaar@users.noreply.github.com>
2020-12-28 14:58:23 +0100

    v

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ const options = [
 ]
 function App() {
 const [country, setCountry] = React.useState('')
+const [country2, setCountry2] = React.useState('')
+const [country3, setCountry3] = React.useState('')
+const [country4, setCountry4] = React.useState('')
 const [numVal, setNumVal] = React.useState('')
 const [option, setOption] = React.useState('')
 const [input, setInput] = React.useState('')
@@ -26,9 +29,9 @@ const [input, setInput] = React.useState('')
     <div className="App">
       <div className="holder">
         <Autocomplete {...params} value={country} setValue={setCountry}/>
-        <Autocomplete {...params} theme={'light'} decoration_clr={'deeppink'} results={15}/> {/* override decoration color */}
-        <Autocomplete {...params} theme={'dark'} decoration_clr={'orange'}/>
-        <Autocomplete data={data} /> {/*only data array mandatory*/}
+        <Autocomplete {...params} value={country2} setValue={setCountry2} theme={'light'} decoration_clr={'deeppink'} results={15}/> {/* override decoration color */}
+        <Autocomplete {...params} value={country3} setValue={setCountry3} theme={'dark'} decoration_clr={'orange'}/>
+        <Autocomplete value={country4} setValue={setCountry4} data={data} /> {/*only data array mandatory*/}
       </div>
       <div className="holder2">
         <RegularInput value={input} setValue={setInput}/>
